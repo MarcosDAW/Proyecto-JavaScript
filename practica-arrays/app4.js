e51478f00fade2596374f758877422c57c2b21a6
MarcosDAW <markos_2008_15@hotmail.com>
2021-09-21 16:43:54 +0200

    agregado metodos

---
+++ practica-arrays/app4.js
@@ -0,0 +1,9 @@
+//metodos
+var nombre = "pepe";
+console.log(nombre.toUpperCase());
+console.log(nombre.indexOf("e"));
+console.log(nombre.replace("p","t"));
+console.log(nombre.substring(0,2));
+console.log(nombre.slice(0,-1));
+console.log(nombre.trim("e"));
+console.log(nombre.startsWith("e"));
