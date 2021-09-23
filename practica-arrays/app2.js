c23ef852aea8927bdf1dc82835326b21e2ff5a1b
MarcosDAW <markos_2008_15@hotmail.com>
2021-09-23 15:36:38 +0200

    ejercicio leer los nombres de un array

--- practica-arrays/app2.js
+++ practica-arrays/app2.js
@@ -2,7 +2,7 @@
 //permite mostrar por pantalla el nombre de los
 //nombres que estan almacenados en una 
 //estructura array como la siguiente
-
+ 
 var nombreClientes = [["Antonio","6666666g"],["Antonia", "66666678F"]];
 for(var i = 0;i<nombreClientes.length; i++){
     for(var j = 0;j<nombreClientes.length; j++){
