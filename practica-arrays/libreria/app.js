579ffb459149f11ac80a74111be07f37c234bd7b
MarcosDAW <markos_2008_15@hotmail.com>
2021-09-28 18:43:20 +0200

    añadido clase app11 y 12 y carpeta libreria

---
+++ practica-arrays/libreria/app.js
@@ -0,0 +1,17 @@
+/*
+Crear un formulario con 2 input y un boton de añadir.
+Dicho formulario me permita añadir libros a una estructura 
+de tipo objeto.
+
+Nota:
+crear otros dos text donde puede comprobar que va añadiendo
+ el contenido de los unput a mi estructura.
+*/
+
+//meterlo en una funcion
+let titulo = document.getElementById("titulo").value;
+let titulo = document.getElementById("autor").value;
+
+//añadir esta informacion a mi estructura
+
+let libreria = {};
