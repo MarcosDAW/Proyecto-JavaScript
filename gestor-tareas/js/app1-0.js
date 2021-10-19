421b7c06b1f2391f78eb8ad629cdd222227247a4
MarcosDAW <markos_2008_15@hotmail.com>
2021-10-19 15:42:24 +0200

    lo visto en video

--- gestor-tareas/js/app1-0.js
+++ gestor-tareas/js/app1-0.js
@@ -1,6 +1,21 @@
-let formulario = document.getElementById("dormulario");
+const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll("#formulario input");
 
 const expreciones = {
-    nombre:/([a-zA-ZÁ-ÿ]+\s){2}[a-zA-Z-]+/;
+    nombre:/([a-zA-ZÁ-ÿ]+\s){2}[a-zA-Z-]+/
 }
+
+if(expreciones.text(inputs)){
+    alert(correcto);
+}else{
+    alert(incorrecto);
+}
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',()=>{
+        console.log("tecla levantada");
+    });
+})
+//cuando se precione submit hacer la funcion
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault();
+});
